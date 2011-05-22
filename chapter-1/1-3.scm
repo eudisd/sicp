bf242654eb7b00a0471b4954ac7ebe5a6695fe7b
Eudis Duran <eudisd@live.com>
2011-05-22 15:33:24 -0400

    Finished problem 1-3.scm

---
+++ chapter-1/1-3.scm
@@ -0,0 +1,7 @@
+(define (proc x y z)
+  (cond ((and (< x y) (< x z))
+         (+ (* y y) (* z z)))
+        ((and (< y x) (< y z))
+         (+ (* x x) (* z z)))
+        ((and (< z x) (< z y))
+         (+ (* x x) (* y y)))))
